efa59fbbc2428ba8509d8f33b2896aa4e39a5f38
Lukas <krizmen97@gmail.com>
2020-07-21 20:19:30 +0200

    First Commit

---
+++ app.js
@@ -0,0 +1,15 @@
+// Selectors
+let button = document.querySelector(".btn");
+let input = document.querySelector(".input");
+let form = document.querySelector("form");
+let todoDiv = document.querySelector(".todo-div");
+let ul = document.querySelector(".ul");
+// Event listeners
+button.addEventListener("click", addTodo);
+
+// Functions
+function addTodo(event) {
+  event.preventDefault();
+
+  // Create LI
+}
