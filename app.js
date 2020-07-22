918cc418a4b6cf476b279cebca117bad17845dc1
Lukas <krizmen97@gmail.com>
2020-07-22 19:57:26 +0200

    Base styling into CSS, add todo functionality

--- app.js
+++ app.js
@@ -1,15 +1,34 @@
 // Selectors
-let button = document.querySelector(".btn");
-let input = document.querySelector(".input");
-let form = document.querySelector("form");
-let todoDiv = document.querySelector(".todo-div");
-let ul = document.querySelector(".ul");
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
 // Event listeners
-button.addEventListener("click", addTodo);
+todoButton.addEventListener("click", addTodo);
 
 // Functions
 function addTodo(event) {
+  // Prevent from form submitting
   event.preventDefault();
-
-  // Create LI
+  // Todo DIV
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
+  //  Create LI
+  const newTodo = document.createElement("li");
+  newTodo.innerText = todoInput.value;
+  newTodo.classList.add("todo-item");
+  todoDiv.appendChild(newTodo);
+  // Completed Button
+  const completedButton = document.createElement("button");
+  completedButton.innerHTML = `<i class="fas fa-check"></i>`;
+  completedButton.classList.add("complete-btn");
+  todoDiv.appendChild(completedButton);
+  // Remove Button
+  const trashButton = document.createElement("button");
+  trashButton.innerHTML = `<i class="fas fa-trash"></i>`;
+  trashButton.classList.add("trash-btn");
+  todoDiv.appendChild(trashButton);
+  // Append TODO DIV into TOTO LIST
+  todoList.appendChild(todoDiv);
 }
+
+// Create LI
